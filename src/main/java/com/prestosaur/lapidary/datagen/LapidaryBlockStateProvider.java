ca2ecf58f80fabdf87dde5e756742b12a6f858ec
Preston Nichols <Prenichols@gmail.com>
2024-02-02 00:13:40 -0500

    Mirrored Mud Blocks

--- src/main/java/com/prestosaur/lapidary/datagen/LapidaryBlockStateProvider.java
+++ src/main/java/com/prestosaur/lapidary/datagen/LapidaryBlockStateProvider.java
@@ -4,14 +4,11 @@ import com.prestosaur.lapidary.Lapidary;
 import com.prestosaur.lapidary.block.LapidaryBlocks;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.WallBlock;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
-import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -45,10 +42,10 @@ public class LapidaryBlockStateProvider extends BlockStateProvider
 
         cubeAllWithItem(LapidaryBlocks.CRACKED_BRICKS);
 
-        cubeAllWithItem(LapidaryBlocks.MOSSY_MUD_BRICKS);
+        cubeMirrorWithItem(LapidaryBlocks.MOSSY_MUD_BRICKS.get());
         triadAllWithItem(LapidaryBlocks.MOSSY_MUD_BRICK_STAIRS, LapidaryBlocks.MOSSY_MUD_BRICK_SLAB, LapidaryBlocks.MOSSY_MUD_BRICK_WALL,
                 new ResourceLocation(Lapidary.MODID, "block/mossy_mud_bricks"));
-        cubeAllWithItem(LapidaryBlocks.CRACKED_MUD_BRICKS);
+        cubeMirrorWithItem(LapidaryBlocks.CRACKED_MUD_BRICKS.get());
 
         triadAllWithItem(LapidaryBlocks.NETHERRACK_STAIRS, LapidaryBlocks.NETHERRACK_SLAB, LapidaryBlocks.NETHERRACK_WALL,
                 new ResourceLocation("block/netherrack"));
@@ -69,7 +66,7 @@ public class LapidaryBlockStateProvider extends BlockStateProvider
     }
 
     // Creates an item from an existing block.
-    private <T extends Block> void simpleBlockItem(RegistryObject<T> block)
+    private <T extends Block> void simpleItemFromBlock(RegistryObject<T> block)
     {
         ResourceLocation id = block.getId();
         itemModels().getBuilder(id.getPath()).parent(models().getExistingFile(id));
@@ -81,7 +78,7 @@ public class LapidaryBlockStateProvider extends BlockStateProvider
         // Do Block Model.
         stairsBlock(stair.get(), texture);
         // Do Item Model.
-        simpleBlockItem(stair);
+        simpleItemFromBlock(stair);
     }
 
     // Creates a slab with an item where all sides are the same.
@@ -90,7 +87,7 @@ public class LapidaryBlockStateProvider extends BlockStateProvider
         // Do Block Model.
         slabBlock(slab.get(), texture, texture);
         // Do Item Model.
-        simpleBlockItem(slab);
+        simpleItemFromBlock(slab);
     }
 
     // Creates a wall with an item where all sides are the same.
@@ -107,6 +104,15 @@ public class LapidaryBlockStateProvider extends BlockStateProvider
         stairAllWithItem(stair, texture);
         slabAllWithItem(slab, texture);
         wallAllWithItem(wall, texture);
+    }
+
+    private void cubeMirrorWithItem(Block block)
+    {
+        String name = ForgeRegistries.BLOCKS.getKey(block).getPath();
+        ModelFile allModel = cubeAll(block);
+        ModelFile mirrorModel = models().singleTexture(name + "_north_west_mirrored", new ResourceLocation(ModelProvider.BLOCK_FOLDER + "/cube_north_west_mirrored_all"), "all", blockTexture(block));
+        simpleBlock(block, mirrorModel);
+        simpleBlockItem(block, allModel);
     }
 }
 
