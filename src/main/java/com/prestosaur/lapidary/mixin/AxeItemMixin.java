f0d468ee45ec90d14f3b55d0390110968acd6f7d
Preston Nichols <Prenichols@gmail.com>
2024-02-11 18:09:35 -0500

    Simple AxeItemMixin

---
+++ src/main/java/com/prestosaur/lapidary/mixin/AxeItemMixin.java
@@ -0,0 +1,21 @@
+package com.prestosaur.lapidary.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.context.UseOnContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AxeItem.class)
+public class AxeItemMixin {
+    @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
+    public void useOnMixin(UseOnContext pContext, CallbackInfoReturnable<InteractionResult> info) {
+        Player player = pContext.getPlayer();
+        if(pContext.getHand() == InteractionHand.MAIN_HAND && !player.getItemInHand(InteractionHand.OFF_HAND).isEmpty())
+            info.setReturnValue(InteractionResult.PASS);
+    }
+}
