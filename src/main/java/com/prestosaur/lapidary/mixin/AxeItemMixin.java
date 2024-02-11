363d3ad19a8503e3ae2f54afb866f39c7be28a4d
Preston Nichols <Prenichols@gmail.com>
2024-02-11 00:26:51 -0500

    Tweaked Axe Stripping

---
+++ src/main/java/com/prestosaur/lapidary/mixin/AxeItemMixin.java
@@ -0,0 +1,22 @@
+package com.prestosaur.lapidary.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.context.UseOnContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AxeItem.class)
+public abstract class AxeItemMixin
+{
+    @Inject(method="useOn", at = @At("HEAD"), cancellable = true)
+    public void useOnMixin(UseOnContext pContext, CallbackInfoReturnable<InteractionResult> info)
+    {
+        var player = pContext.getPlayer();
+        if(pContext.getHand() == InteractionHand.MAIN_HAND && !player.getItemInHand(InteractionHand.OFF_HAND).isEmpty())
+            info.setReturnValue(InteractionResult.PASS);
+    }
+}
