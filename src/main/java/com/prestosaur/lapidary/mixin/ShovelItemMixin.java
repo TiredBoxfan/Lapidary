e46d2faf35c7422e07cd1d2a234732c59a9d5304
Preston Nichols <Prenichols@gmail.com>
2024-02-18 13:49:05 -0500

    Shovel Mixin Functionality

--- src/main/java/com/prestosaur/lapidary/mixin/ShovelItemMixin.java
+++ src/main/java/com/prestosaur/lapidary/mixin/ShovelItemMixin.java
@@ -1,34 +1,45 @@
 package com.prestosaur.lapidary.mixin;
 
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import com.prestosaur.lapidary.enchantment.LapidaryEnchantments;
+import com.prestosaur.lapidary.enchantment.SculptEnchantment;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
-import org.checkerframework.checker.units.qual.A;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static net.minecraftforge.common.ForgeMod.BLOCK_REACH;
+
 @Mixin(ShovelItem.class)
 public abstract class ShovelItemMixin extends Item {
+
+    // TODO: What is the correct way?
+    @Nullable
+    @Unique
+    BlockPos lapidary$lastTargetPos;
+
     public ShovelItemMixin(Properties pProperties) {
         super(pProperties);
     }
-
-    // How far the secondary uses of the axe can be used.
-    // Referenced from BrushItem.
-    //@Unique
-    //protected static final double MAX_SCULPT_DISTANCE = Math.sqrt(ServerGamePacketListenerImpl.MAX_INTERACTION_DISTANCE) - 1.0D;
 
     @Override
     public @NotNull UseAnim getUseAnimation(@NotNull ItemStack pStack) {
@@ -37,13 +48,63 @@ public abstract class ShovelItemMixin extends Item {
 
     @Override
     public int getUseDuration(@NotNull ItemStack pStack) {
-        return 120; // TODO: Determine ideal value.
+        return 32; // TODO: Determine ideal value.
+    }
+
+    // Referenced from BrushItem.
+    @Unique
+    private HitResult lapidary$calculateHitResult(LivingEntity pEntity) {
+        return ProjectileUtil.getHitResultOnViewVector(pEntity, (p) -> !p.isSpectator() && p.isPickable(), pEntity instanceof Player player ? player.getBlockReach() : BLOCK_REACH.get().getDefaultValue());
     }
 
     @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
     public void useOnMixin(UseOnContext pContext, CallbackInfoReturnable<InteractionResult> info) {
+
         Player player = pContext.getPlayer();
-        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, pContext.getPlayer()) > 0 || (pContext.getHand() == InteractionHand.MAIN_HAND && !player.getItemInHand(InteractionHand.OFF_HAND).isEmpty()))
+        BlockPos blockpos = pContext.getClickedPos();
+
+        if (player == null) {
             info.setReturnValue(InteractionResult.PASS);
+            return;
+        }
+
+        System.out.println("Called ShovelItemMixin.useOnMixin - Secondary Use: " + player.isUsingItem());
+
+        if (EnchantmentHelper.getEnchantmentLevel(LapidaryEnchantments.SCULPT.get(), player) <= 0 && !player.isUsingItem()) {
+
+            if (EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, player) > 0 || (pContext.getHand() == InteractionHand.MAIN_HAND && !player.getItemInHand(InteractionHand.OFF_HAND).isEmpty())) {
+                info.setReturnValue(InteractionResult.PASS);
+                return;
+            }
+
+            System.out.println("Not cancelled.");
+
+            lapidary$lastTargetPos = blockpos;
+            player.startUsingItem(pContext.getHand());
+
+            info.setReturnValue(InteractionResult.CONSUME);
+        }
+    }
+
+    @Override
+    public void onUseTick(@NotNull Level pLevel, @NotNull LivingEntity pLivingEntity, @NotNull ItemStack pStack, int pRemainingUseDuration) {
+        if (pLivingEntity instanceof Player player && pRemainingUseDuration >= 0) {
+            // Get the targeted block information, if any.
+            HitResult hitResult = lapidary$calculateHitResult(pLivingEntity);
+            if (hitResult instanceof BlockHitResult blockHitResult) {
+                // Ensure the same block is being targeted.
+                BlockPos blockpos = blockHitResult.getBlockPos();
+                if (blockpos.equals(lapidary$lastTargetPos)) {
+                    if (pRemainingUseDuration <= 1)
+                        useOn(new UseOnContext(player, player.getUsedItemHand(), blockHitResult));
+                } else {
+                    pLivingEntity.releaseUsingItem();
+                }
+            } else {
+                pLivingEntity.releaseUsingItem();
+            }
+        } else {
+            pLivingEntity.releaseUsingItem();
+        }
     }
 }
