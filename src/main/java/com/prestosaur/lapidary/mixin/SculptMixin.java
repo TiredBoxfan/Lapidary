5a5c14ca5da32fdcf56ce12dee3c1aa94743865a
Preston Nichols <Prenichols@gmail.com>
2024-02-18 21:49:25 -0500

    Sculpt Particles

--- src/main/java/com/prestosaur/lapidary/mixin/SculptMixin.java
+++ src/main/java/com/prestosaur/lapidary/mixin/SculptMixin.java
@@ -3,22 +3,26 @@ package com.prestosaur.lapidary.mixin;
 import com.prestosaur.lapidary.Config;
 import com.prestosaur.lapidary.accessors.PlayerSculptAccessor;
 import com.prestosaur.lapidary.enchantment.LapidaryEnchantments;
+import com.prestosaur.lapidary.util.SculptParticlesDelta;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
-import net.minecraft.world.item.DiggerItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -104,7 +108,6 @@ public abstract class SculptMixin extends Item {
     //@Unique
     //public void lapidary$onUseTickSculpt(@NotNull Level pLevel, @NotNull LivingEntity pLivingEntity, @NotNull ItemStack pStack, int pRemainingUseDuration) {
     public void onUseTick(@NotNull Level pLevel, @NotNull LivingEntity pLivingEntity, @NotNull ItemStack pStack, int pRemainingUseDuration) {
-        System.out.println("onUseTick");
         if (pLivingEntity instanceof Player player && pRemainingUseDuration >= 0) {
             // Get the targeted block information, if any.
             HitResult hitResult = lapidary$calculateHitResult(pLivingEntity);
@@ -115,6 +118,9 @@ public abstract class SculptMixin extends Item {
                     if (pRemainingUseDuration == 1) { // Only use when exactly 1 remaining use ticks are left.
                         useOn(new UseOnContext(player, player.getUsedItemHand(), blockHitResult));
                         pLivingEntity.releaseUsingItem();
+                    } else if (pRemainingUseDuration % 10 == 5) {
+                        HumanoidArm arm = pLivingEntity.getUsedItemHand() == InteractionHand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
+                        this.lapidary$spawnDustParticles(pLevel, blockHitResult, pLevel.getBlockState(blockpos), pLivingEntity.getViewVector(0.0F), arm);
                     }
                 } else {
                     // Release due to a different block being targeted.
@@ -128,5 +134,21 @@ public abstract class SculptMixin extends Item {
             // Release because action is not done by a player.
             pLivingEntity.releaseUsingItem();
         }
+    }
+
+    // Borrowed from BrushItem.
+    @Unique
+    public void lapidary$spawnDustParticles(Level pLevel, BlockHitResult pHitResult, BlockState pState, Vec3 pPos, HumanoidArm pArm) {
+        int i = pArm == HumanoidArm.RIGHT ? 1 : -1;
+        int j = pLevel.getRandom().nextInt(7, 12);
+        BlockParticleOption blockparticleoption = new BlockParticleOption(ParticleTypes.BLOCK, pState);
+        Direction direction = pHitResult.getDirection();
+        SculptParticlesDelta particlesDelta = SculptParticlesDelta.fromDirection(pPos, direction);
+        Vec3 vec3 = pHitResult.getLocation();
+
+        for(int k = 0; k < j; ++k) {
+            pLevel.addParticle(blockparticleoption, vec3.x - (double)(direction == Direction.WEST ? 1.0E-6F : 0.0F), vec3.y, vec3.z - (double)(direction == Direction.NORTH ? 1.0E-6F : 0.0F), particlesDelta.xd() * (double)i * 3.0D * pLevel.getRandom().nextDouble(), 0.0D, particlesDelta.zd() * (double)i * 3.0D * pLevel.getRandom().nextDouble());
+        }
+
     }
 }
---
+++ src/main/java/com/prestosaur/lapidary/util/SculptParticlesDelta.java
@@ -0,0 +1,21 @@
+package com.prestosaur.lapidary.util;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.Vec3;
+
+public record SculptParticlesDelta(double xd, double yd, double zd) {
+    private static final double ALONG_SIDE_DELTA = 1.0D;
+    private static final double OUT_FROM_SIDE_DELTA = 0.1D;
+
+    public static SculptParticlesDelta fromDirection(Vec3 pPos, Direction pDirection) {
+
+        return switch (pDirection) {
+            case DOWN, UP -> new SculptParticlesDelta(pPos.z(), 0.0D, -pPos.x());
+            case NORTH -> new SculptParticlesDelta(ALONG_SIDE_DELTA, 0.0D, -OUT_FROM_SIDE_DELTA);
+            case SOUTH -> new SculptParticlesDelta(-ALONG_SIDE_DELTA, 0.0D, OUT_FROM_SIDE_DELTA);
+            case WEST -> new SculptParticlesDelta(-OUT_FROM_SIDE_DELTA, 0.0D, -ALONG_SIDE_DELTA);
+            case EAST -> new SculptParticlesDelta(OUT_FROM_SIDE_DELTA, 0.0D, ALONG_SIDE_DELTA);
+            default -> throw new IncompatibleClassChangeError();
+        };
+    }
+}
