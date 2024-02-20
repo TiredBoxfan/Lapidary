2767eed1cfe96c1e486ca76c56d3a75830d6303c
Preston Nichols <Prenichols@gmail.com>
2024-02-19 19:14:39 -0500

    Added More Crackable Blocks

--- src/main/java/com/prestosaur/lapidary/sculpt/LapidarySculpt.java
+++ src/main/java/com/prestosaur/lapidary/sculpt/LapidarySculpt.java
@@ -1,6 +1,7 @@
 package com.prestosaur.lapidary.sculpt;
 
 import com.google.common.collect.ImmutableMap;
+import com.prestosaur.lapidary.block.LapidaryBlocks;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
@@ -9,5 +10,15 @@ import java.util.Map;
 public class LapidarySculpt {
     public static final Map<Block, Block> CRACKABLES = (new ImmutableMap.Builder<Block, Block>())
             .put(Blocks.STONE, Blocks.COBBLESTONE)
+            .put(Blocks.STONE_BRICKS, Blocks.CRACKED_STONE_BRICKS)
+            .put(Blocks.DEEPSLATE, Blocks.COBBLED_DEEPSLATE)
+            .put(Blocks.DEEPSLATE_BRICKS, Blocks.CRACKED_DEEPSLATE_BRICKS)
+            .put(Blocks.DEEPSLATE_TILES, Blocks.CRACKED_DEEPSLATE_TILES)
+            .put(Blocks.BRICKS, LapidaryBlocks.CRACKED_BRICKS.get())
+            .put(Blocks.MUD_BRICKS, LapidaryBlocks.CRACKED_MUD_BRICKS.get())
+            .put(Blocks.POLISHED_BLACKSTONE_BRICKS, Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS)
+            .put(Blocks.NETHER_BRICKS, Blocks.CRACKED_NETHER_BRICKS)
+            .put(Blocks.END_STONE_BRICKS, LapidaryBlocks.CRACKED_END_STONE_BRICKS.get())
+            .put(Blocks.PURPUR_BLOCK, LapidaryBlocks.CRACKED_PURPUR_BLOCK.get())
             .build();
 }
